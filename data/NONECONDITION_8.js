9c8085a4068ab1e3b534b4cd417bb8a1bd6756e8
Mieboymesias <161265927+Mieboymesias@users.noreply.github.com>
2024-06-14 13:48:29 +0800

    Inventory
    Inventory

---
+++ data/NONECONDITION_8.js
@@ -0,0 +1,8 @@
+var json_NONECONDITION_8 = {
+"type": "FeatureCollection",
+"name": "NONECONDITION_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "photo": "D:\\GIS INVENTORY\\NONE\\NONOC MULTI PURPOSE BUILDING ELEMENTARY.jpg", "filename": "NONOC MULTI PURPOSE BUILDING ELEMENTARY", "directory": "D:\\GIS INVENTORY\\NONE", "altitude": null, "direction": null, "longitude": "125.58116197222222", "latitude": "9.820447027777778", "timestamp": null }, "geometry": { "type": "Point", "coordinates": [ 125.581161972222219, 9.820447027777778 ] } }
+]
+}
